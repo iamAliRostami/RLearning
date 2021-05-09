44c536027f32b5b5edb3fe1a651f7152e9e8b7ca
iamAliRostami <ali.rostami33@gmail.com>
2021-05-09 09:40:04 +0430

    First commit...

---
+++ app/src/main/java/com/leon/r_learning/activities/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.leon.r_learning.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leon.r_learning.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+    ActivityHomeBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
---
+++ app/src/main/java/com/leon/r_learning/layout/SquareLayout.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2018 Gabriele Corso
+ *
+ *  Distributed under the MIT software license, see the accompanying
+ *  file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+ */
+
+package com.leon.r_learning.layout;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+/**
+ * Dynamic layout definition in order to make the items in the genres grid squared
+ */
+
+public class SquareLayout extends RelativeLayout {
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
